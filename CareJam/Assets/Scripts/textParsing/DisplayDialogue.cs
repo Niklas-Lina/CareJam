d5396a8b6dc5a9b9ace7a2f7fc5cbcfbf557ede7
LinaLundstedt <lundstedt.lina@hotmail.com>
2021-11-29 22:02:02 -0300

    med pixelshader och text parsing scripts
    Lagt in gamla script fran skol-spelet, hann inte kommentera vad allt ar, men du kanske forstar anda

---
+++ CareJam/Assets/Scripts/textParsing/DisplayDialogue.cs
@@ -0,0 +1,275 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayDialogue : MonoBehaviour
+{
+    public static DisplayDialogue DispD;
+    GameObject Kid;
+    [HideInInspector] public conflict CD;
+    Text KidsTxt;
+    public RectTransform BubbleTrans1;
+    public RectTransform BubbleTrans2;
+    RectTransform currentBubble;
+    public GameObject AnswerPrefab;
+    public GameObject PlayerPanel;
+
+    AudioSource Audio;
+    public AudioClip ClientVoice;
+    public AudioClip PlayerVoice;
+    public Image readyButton;
+
+    GameObject startQ;
+
+    bool txtDone = true;
+    bool KidAnswer = false;
+    string nextSentence = string.Empty;
+    int currentSenNr = 0;
+    int fullSentanceNr = -1;
+
+    Nodes currentNode;
+
+    bool canClick = true;
+
+    private void Awake()
+    {
+
+        if (DispD == null)
+        { DispD = this; }
+        else if (DispD != this)
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+
+
+        Audio = gameObject.GetComponent<AudioSource>();
+
+    }
+
+    private void Start()
+    {
+
+    }
+
+
+    private void Update()
+    {
+        readyButton.enabled = canClick;
+
+        if (Kid != null)
+        {
+            if (KidAnswer && !txtDone)
+            {
+                if (Input.GetMouseButtonDown(0) && canClick)
+                {
+                    currentSenNr += 1;
+
+                    if (currentSenNr >= fullSentanceNr)
+                    {
+                        txtDone = true;
+                        Question(currentNode);
+                        KidAnswer = false;
+                        currentSenNr = 0;
+                    }
+                    else
+                    {
+                        Debug.Log(1);
+                        nextSentence = SentenceCut(currentSenNr, currentNode.KidTxt);
+                        setBubble(nextSentence);
+                        StartCoroutine(TypeText(nextSentence, KidsTxt, null));
+                    }
+
+                }
+
+            }
+        }
+
+
+    }
+
+    public void AskQuestion(string question, string path)
+    {
+
+        Nodes nextNode = null;
+        GameObject q = createQuestion(question);
+        Debug.Log(q);
+
+        if (q != null)
+        {
+            for (int i = 0; i < CD.pathNodes.Count; i++)
+            {
+                if (CD.pathNodes[i].pathTitle.Contains(path))
+                {
+                    nextNode = CD.pathNodes[i];
+                }
+            }
+
+            q.GetComponent<Button>().onClick.AddListener(() => { SetPath(nextNode); });
+        }
+    }
+
+    public void SetPath(Nodes node)
+    {
+        foreach (Transform child in PlayerPanel.transform)
+        {
+            GameObject.Destroy(child.gameObject);
+        }
+
+        CD.currentNode = node;
+        Debug.Log(node);
+
+        if (node.KidTxt.Contains("<"))
+        {
+            currentBubble.gameObject.SetActive(false);
+            string result = node.KidTxt.Split('<', '"')[1];
+            string name = node.KidTxt.Split('"', '"')[1];
+            int nr = CD.SessionNr;
+
+            //Det har var for att avsluta det spelet
+            /*
+            EndofGame.End ending = new EndofGame.End { endResult = result, KidName = name, SessionNr = nr };
+            Ending.endings.Add(ending);
+            currentBubble.gameObject.SetActive(false);
+
+            GC.Talking = false;*/
+
+        }
+        else
+        {
+            currentNode = node;
+
+            KidAnswer = true;
+
+            nextSentence = SentenceCut(currentSenNr, currentNode.KidTxt);
+            setBubble(nextSentence);
+            StartCoroutine(TypeText(nextSentence, KidsTxt, null));
+        }
+    }
+
+
+    string SentenceCut(int nextnr, string Wholetxt)
+    {
+        string chop = Wholetxt;
+        string[] sentances = chop.Split('*');
+        fullSentanceNr = sentances.Length;
+
+        return sentances[nextnr];
+
+    }
+
+    void StartSession(conflict s)
+    {
+        Destroy(startQ);
+
+        txtDone = false;
+        Debug.Log(CD.currentNode);
+        SetPath(CD.currentNode);
+
+    }
+
+
+    void Question(Nodes node)
+    {
+        for (int i = 0; i < CD.currentNode.questions.Count; i++)
+        {
+            AskQuestion(node.questions[i].question, node.questions[i].destination);
+        }
+    }
+
+
+    GameObject createQuestion(string Questiontxt)
+    {
+        GameObject q = Instantiate(AnswerPrefab, PlayerPanel.transform.position, Quaternion.identity) as GameObject;
+        q.transform.SetParent(PlayerPanel.transform);
+        q.GetComponentInChildren<Text>().text = Questiontxt;
+        return q;
+    }
+
+
+    IEnumerator TypeText(string message, Text txtObj, AudioClip voice)
+    {
+        canClick = false;
+        txtDone = false;
+        int index = 0;
+        string colorTag = "<color=#00000000>";
+        //Ifall 
+        /*
+        if (Faces != null)
+        { Faces.talking = true; } */
+        float pause = 0.03f;
+        txtObj.text = "";
+        //Bubbleresize(message.Length + 1, trans);
+
+        if (voice != null)
+        {
+            Audio.clip = voice;
+            Audio.Play();
+        }
+
+        while (index <= message.Length)
+        {
+            string txt = message.Substring(0, index) + colorTag + message.Substring(index) + "</color>";
+            txtObj.text = txt;
+            index++;
+
+            if (txt.EndsWith(".") || txt.EndsWith("."))
+            {
+                pause = 0.5f;
+
+            }
+            else
+                pause = 0.03f;
+
+            yield return new WaitForSeconds(pause);
+        }
+
+        canClick = true;
+        //Faces.talking = false;
+        currentSenNr += 1;
+        yield return 0;
+    }
+
+
+
+    public void SetKid(GameObject kid)
+    {
+        Kid = kid;
+        CD = Kid.GetComponent<KidDialogueData>().currentConflict;
+
+        //Att se om nagon "pratar"
+        /*
+        if (!GC.Talking)
+        {
+            GC.Talking = true;
+            startQ = createQuestion(CD.Opener);
+            startQ.GetComponent<Button>().onClick.AddListener(delegate { StartSession(CD); });
+            CD.currentNode = CD.pathNodes[0];
+            currentNode = CD.pathNodes[0];
+            Debug.Log(CD.pathNodes[0].KidTxt);
+        }*/
+
+    }
+
+    //Se till att ratt pratbubbla anvands
+    void setBubble(string text)
+    {
+        if (currentBubble != null)
+        { currentBubble.gameObject.SetActive(false); }
+
+        if (text[0] == '&')
+        {
+            currentBubble = BubbleTrans1;
+            
+        }
+        else if (text[0] == '%')
+        {
+            currentBubble = BubbleTrans2;
+        }
+        KidsTxt = currentBubble.GetComponentInChildren<Text>();
+        currentBubble.gameObject.SetActive(true);
+        readyButton.transform.SetParent(currentBubble);
+    }
+
+}
---
+++ CareJam/Assets/Scripts/textParsing/KidDialogueData.cs
@@ -0,0 +1,197 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using UnityEngine.UI;
+using System;
+using System.Text.RegularExpressions;
+
+public class KidDialogueData : MonoBehaviour
+{
+    public TextAsset twineText;
+    public int currentSessionNR = 1;
+    public conflict currentConflict;
+
+
+    void ParseText(TextAsset txt)
+    {
+
+        //PARSE TO NODES
+
+        string text = Regex.Replace(txt.text, "{.*?}", string.Empty);
+        string[] blocks = text.Split(':');
+
+        List<string> pathTtitles = new List<string>();
+        List<string> responses = new List<string>();
+        List<string> destinations = new List<string>();
+        List<string> KidTxts = new List<string>();
+        List<Nodes> allNodes = new List<Nodes>();
+
+        string CurrentLine;
+
+
+        for (int i = 0; i < blocks.Length; i++)
+        {
+            //If textblock is not empty
+            if (blocks[i].Length > 0)
+            {
+                //create a new node for this textblock
+                Nodes node = new Nodes();
+
+
+                //GET QUESTION
+                if (blocks[i].Contains('['))
+                {
+
+                    string[] Questions = blocks[i].Split('[', '\n');
+
+                    string thisQuestion = string.Empty;
+
+                    string thisDestination = string.Empty;
+
+                    for (int f = 4; f<Questions.Length; f++ )
+                    {
+
+                        if (Questions[f].Contains("]]") || Questions[f].Contains("if $"))
+                        {
+
+                            if (Questions[f].Contains("]]"))
+                            {
+                                //SUPER UGLY, fix when not tired
+                                CurrentLine = Questions[f].Remove(Questions[f].Length - 3);
+                                //split between | to get both question and destination
+                                thisQuestion = CurrentLine.Split('|', '\n')[0];
+
+                                CurrentLine = Questions[f].Remove(Questions[f].Length - 3);
+                                //last one doesn't have a destination, ignore that one
+                                if (CurrentLine.Contains("|"))
+                                    thisDestination = CurrentLine.Split('|', '\n')[1];
+
+
+
+                                //No empty questions
+                                if (thisQuestion != string.Empty)
+                                {
+                                    Question question = new Question();
+                                    question.question = thisQuestion;
+                                    question.destination = thisDestination;
+
+                                    node.questions.Add(question);
+
+                                }
+
+
+                            }
+
+                        }
+                    }
+
+                }
+
+                //GET TITLE AND KID TEXT
+
+                CurrentLine = blocks[i].Split('\n')[0];
+                //make sure text has some text
+                if (CurrentLine.Length > 1)
+                {
+                    node.pathTitle = blocks[i].Split('\n')[0]; 
+                }
+
+
+                //GET SESSION START
+                if (CurrentLine.Contains("="))
+                {
+                        currentConflict.Opener = CurrentLine.Substring(CurrentLine.IndexOf("=") + 1); ;
+                        currentConflict.SessionNr = currentSessionNR;
+
+
+                }
+
+              
+
+                //kid response comes after the titles, one line down
+                CurrentLine = blocks[i].Split('\n')[1];
+
+                //make sure kid is saying something
+                if (CurrentLine.Length > 1)
+                {
+                    node.KidTxt = blocks[i].Split('\n')[1];
+
+                }
+
+
+                //IS THIS A REAL NODE?
+                if ( node.pathTitle != null)
+                {
+                    currentConflict.pathNodes.Add(node);
+
+                }
+
+            }
+
+
+        }
+
+       
+    }
+
+
+    public int GetFirstIntFromString(string Str)
+    {
+        string strNr = string.Empty;
+        int result = -1;
+
+        for (int i = 0; i < Str.Length; i++)
+        {
+            if (Char.IsDigit(Str[i]))
+            {
+                strNr += Str[i];
+                result = int.Parse(strNr);
+                return result;
+            }
+        }
+
+        return result;
+    }
+
+
+    void Awake()
+    {
+        ParseText(twineText);
+    }
+
+}
+
+[Serializable]
+
+
+public class conflict
+{
+    public string Opener;
+    public int SessionNr;
+    //witch pathNode
+    public List<Nodes> pathNodes = new List<Nodes>();
+    public Nodes currentNode;
+    public string Closer;
+
+}
+
+public class Nodes
+{
+    public string pathTitle;
+    public string KidTxt;
+    public List<Question> questions = new List<Question>();
+}
+
+public class Question
+{
+    public string question;
+    public string destination;
+    public bool hasData = false;
+}
+
+
+
+
+
+
